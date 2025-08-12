79da3af126a091dfc0bc5d21308716acd6c31034
vinnysts <vinnysts.dg@outlook.com.br>
2025-08-12 10:41:35 -0300

    Ativação da função sortear amigo, exibição da lista de amigos selecionados e exibição do nome do amigo sorteado

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
+//Define uma lista de nomes sorteados
 let amigosSelecionados = [];
 
+//Função que permite ao botão Adicionar, incluir nomes a lista de sorteados 
 function adicionarAmigo() {
+     
+    let adicionado = document.querySelector('input').value.trim(); //Seleciona a teg input e garante que seu valor inicial está vazio
     
-    let adicionado = document.querySelector('input').value.trim();
-    if (adicionado === ""){
+    if (adicionado === ""){ //Exibe erro em caso de valor vazio
 
         return mensagemResposta('listaAmigos', 'Digite um nome!');
 
-    }else if(amigosSelecionados.includes(adicionado)) {
+    }else if(amigosSelecionados.includes(adicionado)) { //Exibe erro caso o nome já tenha sido adicionado a lista, esvaziando o input novamente
 
         document.querySelector('input').value = '';
 
-        return mensagemResposta('listaAmigos', 'Nome já adicionado!');
+        return mensagemResposta('listaAmigos', 'Amigo já adicionado!');
 
     }else{
-        adicionado = document.querySelector('input').value;
+        adicionado = document.querySelector('input').value; //Adiciona novos nomes a lista
 
         amigosSelecionados.push(adicionado);
 
         document.querySelector('input').value = '';
 
-        console.log(amigosSelecionados , amigosSelecionados.length);
+        return mensagemResposta('listaAmigos', amigosSelecionados);
     }
-
 };
 
-function sortearAmigo() {    
+//Adiciona a função de sorteio ao botão Sortear amigo
+function sortearAmigo() {  
+
+    let indiceAmigos = Math.floor(Math.random() * amigosSelecionados.length);
+    let sorteado = amigosSelecionados[indiceAmigos];
+
+    return mensagemResposta('resultado', sorteado);
 };
 
+//Retorna mensagens de erro
 function mensagemResposta(id, texto) {
-    let campo = document.getElementById('listaAmigos');
+    let campo = document.getElementById(id);
     campo.innerHTML = texto;
-}
-
-console.log(amigosSelecionados.length);
+};
