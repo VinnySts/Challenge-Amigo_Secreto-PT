6b9bd17c4525014161d27c41aa6a13090cd63e50
vinnysts <vinnysts.dg@outlook.com.br>
2025-08-12 15:06:44 -0300

    Inclui separador entre os nome de amigos listados, calcula quantidade de vezes que o botão Sorteiar amigos foi acionado

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//Define uma lista de nomes sorteados
-let amigosSelecionados = [];
+let amigosSelecionados = []; //Define uma lista de nomes sorteados
+let contadorSorteios = 0; //Contabiliza o número de vezes que o botão Sortear amigo foi acionado
 
 //Função que permite ao botão Adicionar, incluir nomes a lista de sorteados 
 function adicionarAmigo() {
      
-    let adicionado = document.querySelector('input').value.trim(); //Seleciona a teg input e garante que seu valor inicial está vazio
+    let adicionado = document.querySelector('input').value.trim(); //Seleciona a tag input e garante que seu valor inicial está vazio
     
     if (adicionado === ""){ //Exibe erro em caso de valor vazio
 
@@ -23,17 +23,23 @@ function adicionarAmigo() {
 
         document.querySelector('input').value = '';
 
-        return mensagemResposta('listaAmigos', amigosSelecionados);
+        return mensagemResposta('listaAmigos', amigosSelecionados.join(' - '));//inclui separador na lista de amigos
     }
 };
 
 //Adiciona a função de sorteio ao botão Sortear amigo
-function sortearAmigo() {  
+function sortearAmigo() {
 
     let indiceAmigos = Math.floor(Math.random() * amigosSelecionados.length);
     let sorteado = amigosSelecionados[indiceAmigos];
 
-    return mensagemResposta('resultado', sorteado);
+    if (amigosSelecionados.length === 0) { //Corrige bug que exibia a mensagem caso Sortear amigo fosse clicado sem nomes na lista
+        return mensagemResposta('listaAmigos', 'Digite um nome!');
+    }
+
+    contadorSorteios++; //Atribui mais um a contagem, toda vezes que o botão Sortear amigo foi acionado
+  
+    return mensagemResposta('resultado', `O amigo secreto sorteado é: ${sorteado} (${contadorSorteios}º sorteio).`);
 };
 
 //Retorna mensagens de erro
